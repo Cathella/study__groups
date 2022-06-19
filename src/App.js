3446084ae638d9135bfad57feb6c7c9670b0b4a3
Cathella <ncathie214@gmail.com>
2022-06-19 06:45:33 +0300

    Complete home component styling

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ import Nav from './components/Nav';
 const App = () => {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="teachers/login" element={<TeacherLogin />} />
-        <Route path="teachers/signup" element={<TeacherSignup />} />
-        <Route path="teachers/dashboard" element={<TeacherDashboard />} />
-        <Route path="teachers/profile" element={<TeacherProfile />} />
-        <Route path="teachers/newgroup" element={<NewGroup />} />
-        <Route path="students/signup" element={<StudentSignup />} />
-        <Route path="students/login" element={<StudentLogin />} />
-      </Routes>
+      <div className="container">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="teachers/login" element={<TeacherLogin />} />
+          <Route path="teachers/signup" element={<TeacherSignup />} />
+          <Route path="teachers/dashboard" element={<TeacherDashboard />} />
+          <Route path="teachers/profile" element={<TeacherProfile />} />
+          <Route path="teachers/newgroup" element={<NewGroup />} />
+          <Route path="students/signup" element={<StudentSignup />} />
+          <Route path="students/login" element={<StudentLogin />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,53 @@
 import React from 'react'
+import HeaderImg from '../img/header-img.svg'
+import ArrowYellow from '../img/arrow-yellow.svg'
+import SearchIcon from '../img/Search.svg'
+import UserIcon from '../img/User.svg'
+import ArrowBlack from '../img/arrow-black.svg'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className='home-container'>
+      <section className='desc-section'>
+        <div className='heading'>
+          <h1>A space for Students to join Study Groups near them.</h1>
+          <img src={ArrowYellow} alt='' />
+        </div>
+        <div>
+          <ul>
+            <li>As a parent watch your child’s confidence build step by step.</li>
+            <li>Students will learn how to learn, not just how to write exams</li>
+            <li>Celebrate life-changing results & happy students.</li>
+          </ul>
+        </div>
+        <div className='home-search-section'>
+          <div className='home-form'>
+            <form>
+              <label>
+                <span>
+                  <img src={UserIcon} alt='' />
+                </span>
+                <span className='text'>Search for Groups</span>
+              </label>
+              <div className='group-search'>
+                <input type='text' placeholder='Search for Subject, Grade or Location' />
+                <button type='submit'>
+                  <img src={SearchIcon} alt='' />
+                </button>
+              </div>
+            </form>
+          </div>
+          <img src={ArrowBlack} alt='' className='arrow' />
+        </div>
+      </section>
+      <section className='header-img'>
+        <img src={HeaderImg} alt='' />
+      </section>
+    </div>
+    <footer>
+      <span>&copy; 2022 Study Groups &middot; Nakitto Creations</span>
+    </footer>
+    </>
   )
 }
